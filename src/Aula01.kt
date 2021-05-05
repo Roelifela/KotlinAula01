0c73c71d7c2473c3a1b3c9af38663063772ea748
Roelifela <rodrigo_cordeiro@hotmail.com>
2021-05-05 19:54:42 -0300

    First commit from Intellij

---
+++ src/Aula01.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    val inteiro = 1
+    val numeroGrande = 1L
+    val numeroGrande2 = 123456789098765432
+    val inteiro1: Int = 1
+    val numeroGrande3: Long = 7654321
+    val texto = "Rodrigo"
+    val texto1: String = "Cordeiro"
+    val ehMaior = true
+    val ehMaior1: Boolean = false
+
+    println(5 == 5)
+
+
+
+
+}
